5a0e199a57605a421200bba937fa45578b3b7a56
BenTuvia96 <bentu@post.bgu.ac.il>
2025-01-05 15:36:41 +0200

    Update comment in server.py to include additional context

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-#hello
+#hello 234
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
