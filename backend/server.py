887e227a7360db82f304e9d302de038563890197
BenTuvia96 <bentu@post.bgu.ac.il>
2025-01-05 17:04:57 +0200

    LIstening to the Azure env port

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
 #hello 2345678
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -36,4 +37,5 @@ def login():
         return jsonify({'message': 'Server error'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Added debug=True for development
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=True)
